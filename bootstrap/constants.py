8d13451a90d3b8ba2aeb81a51a3bce6a0b70545f
asuka <asukanitro@gmail.com>
2026-07-28 21:37:04 +0300

    feat: switch to Cloudflare tunnel launcher.ganj4craft.ru

--- bootstrap/constants.py
+++ bootstrap/constants.py
@@ -3,7 +3,7 @@ GanjaCraft Launcher Bootstrap Constants
 Единая точка конфигурации URL и констант загрузчика
 """
 
-BASE_URL = "https://gcrlauncher1.loca.lt"
+BASE_URL = "https://launcher.ganj4craft.ru"
 FILES_BASE = f"{BASE_URL}/api/launcher/files"
 
 BOOTSTRAP_JSON_URL = f"{FILES_BASE}/bootstrap.json"
--- client/src/main-process/constants.js
+++ client/src/main-process/constants.js
@@ -9,7 +9,7 @@ const MC_VERSION = '1.21.1';
 const REQUIRED_JAVA_MAJOR = 21;
 
 // URLs
-const BASE_URL = 'https://gcrlauncher1.loca.lt';
+const BASE_URL = 'https://launcher.ganj4craft.ru';
 const MIRROR_BASE = `${BASE_URL}/mirror`;
 const FILES_BASE = `${BASE_URL}/files`;
 const API_BASE = `${BASE_URL}/api`;
